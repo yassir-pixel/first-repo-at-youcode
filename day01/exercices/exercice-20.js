9ae971f3a4cfb7d028b54b225982079bbc266abf
yassir-pixel <yassir.taous42@gmail.com>
2026-09-08 16:18:09 +0100

    feat: ajout de la fonctionnalité

--- day01/exercices/exercice-20.js
+++ day01/exercices/exercice-20.js
@@ -18,3 +18,9 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+
+let nom = null;
+let pseudo = "NinjaCoder";
+let nomAAfficher;
+
+console.log(nomAAfficher = nom ?? pseudo ?? "Anonyme")
