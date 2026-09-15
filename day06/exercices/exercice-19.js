322c3f8bdbd996dd56a0cd5b6a76a00cd6d4428d
yassir-pixel <yassir.taous42@gmail.com>
2026-09-15 16:14:26 +0100

    rajout exercice 19 avec un petit beug

--- day06/exercices/exercice-19.js
+++ day06/exercices/exercice-19.js
@@ -15,3 +15,36 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+let tab = [
+    {
+        nom: "A",
+        groupe: 1
+    },
+    {
+        nom: "B",
+        groupe: 2
+    },
+    {
+        nom: "C",
+        groupe: 1
+    }
+]
+
+function arrangerTableau(tab){
+    let retVal = {}
+
+    for (let i = 0; i < tab.length; i++) {
+        let objectGroupe = tab[i].groupe
+
+        if(!retVal[tab[i].groupe])
+            retVal[tab[i].groupe] = []
+        retVal[tab[i].groupe].push({nom: tab[i].nom})
+    }
+
+    return retVal;
+}
+
+
+let tabArranger = arrangerTableau(tab)
+
+console.log(tabArranger) // wierd
